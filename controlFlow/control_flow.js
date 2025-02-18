96be741579ef9238d6522b06486b1d42fa435cba
ADEWALE AFEEZ ALATISE <Alatise37@gmail.com>
2025-02-18 05:40:31 +0100

    message

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,8 @@
+// Declare the isAuthenticated variable and initialize it with the boolean value true
+let isAuthenticated = true;
+
+// Use the ternary operator to set the authenticationStatus
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+// Log the authentication status to the console
+console.log(authenticationStatus);
